16570dfe06bb9b6bb1843cafdbc64cea1b389184
Krzysztof Witalewski <krzysztofwitalewski@workfront.com>
2018-12-16 13:39:27 +0100

    add support for connected components

--- bin/rw.js
+++ bin/rw.js
@@ -9,6 +9,7 @@ program
   .version("1.0.0")
   .option("-n, --name [componentName]", "Component name")
   .option("-p, --path [pathToComponents]", "Path to components direcotry")
+  .option("-c, --connected", "Connect component to redux")
   .parse(process.argv);
 
 const exec = (cmd, ...log) =>
@@ -24,16 +25,33 @@ const exec = (cmd, ...log) =>
     })
   );
 
-const indexJs = name => [`export { ${name} } from \\"./${name}\\";`, ``];
-const componentJs = name => [
+const SKIP = "[[[SKIP]]]";
+
+const indexJs = (name, options) => [
+  `export { ${
+    options.connected ? name + "Connected" : name
+  } } from \\"./${name}\\";`,
+  ``
+];
+const componentJs = (name, options) => [
   `import React from \\"react\\";`,
+  options.connected ? `import { connect } from \\"react-redux\\";` : SKIP,
   `import { ${name}Styled } from \\"./${name}Styled\\";`,
   ``,
   `export const ${name} = () => {`,
+  `  return <${name}Styled></${name}Styled>`,
   `};`,
+  options.connected ? `const mapStateToProps = state => ({` : SKIP,
+  options.connected ? `});` : SKIP,
+  options.connected ? `const mapDispatchToProps = {` : SKIP,
+  options.connected ? `};` : SKIP,
+  options.connected ? `export const ${name}Connected = connect(` : SKIP,
+  options.connected ? `  mapStateToProps,` : SKIP,
+  options.connected ? `  mapDispatchToProps` : SKIP,
+  options.connected ? `)(${name});` : SKIP,
   ``
 ];
-const componentStyledJs = name => [
+const componentStyledJs = (name, options) => [
   `import styled from \\"@emotion/styled\\";`,
   ``,
   `export const ${name}Styled = styled.div\\\``,
@@ -43,31 +61,45 @@ const componentStyledJs = name => [
 
 const writeFile = (name, contents) =>
   exec(
-    `echo "${contents.join("\\n\\\n")}" | cat > ${name}`,
+    `echo "${contents
+      .filter(line => line !== SKIP)
+      .join("\\n\\\n")}" | cat > ${name}`,
     "File",
     chalk.green(name),
     "wriiten."
   );
 
-const writeIndexJs = (name, path) =>
-  writeFile(`${path}/${name}/index.js`, indexJs(name));
-const writeComponentJs = (name, path) =>
-  writeFile(`${path}/${name}/${name}.js`, componentJs(name));
-const writeComponentStyledJs = (name, path) =>
-  writeFile(`${path}/${name}/${name}Styled.js`, componentStyledJs(name));
+const writeIndexJs = (name, options) =>
+  writeFile(`${options.path}/${name}/index.js`, indexJs(name, options));
+const writeComponentJs = (name, options) =>
+  writeFile(`${options.path}/${name}/${name}.js`, componentJs(name, options));
+const writeComponentStyledJs = (name, options) =>
+  writeFile(
+    `${options.path}/${name}/${name}Styled.js`,
+    componentStyledJs(name, options)
+  );
 
-const makeDir = (name, path) =>
-  exec(`mkdir ${path}/${name}`, "Directory", chalk.green(`${path}/${name}`), "created.");
+const makeDir = (name, options) =>
+  exec(
+    `mkdir ${options.path}/${name}`,
+    "Directory",
+    chalk.green(`${options.path}/${name}`),
+    "created."
+  );
 
-const createComponentFiles = (name, path = "src/components") =>
-  makeDir(name, path).then(() =>
+const createComponentFiles = (name, options) =>
+  makeDir(name, options).then(() =>
     Promise.all([
-      writeIndexJs(name, path),
-      writeComponentJs(name, path),
-      writeComponentStyledJs(name, path)
+      writeIndexJs(name, options),
+      writeComponentJs(name, options),
+      writeComponentStyledJs(name, options)
     ]).then(() => console.log("Done."))
   );
 
-const { name, path } = program;
+const { name, connected } = program;
+const path = program.path || "src/components";
 
-createComponentFiles(name, path);
+createComponentFiles(name, {
+  path,
+  connected
+});
