95540becdd90eed5233d6b0bc8ed65fdf9d572b0
Krzysztof Witalewski <krzysztofwitalewski@workfront.com>
2018-12-21 16:18:56 +0100

    refactor createComponent

--- src/createComponent.ts
+++ src/createComponent.ts
@@ -2,33 +2,37 @@ import { of, Task } from "folktale/concurrency/task";
 import { createFileFromTemplate } from "./createFileFromTemplate";
 import { makeDirsForPath } from "./makeDirsForPath";
 
-export const createComponent: (data: componentData) => Task = data => {
-  const tasks = [
-    createFileFromTemplate(
-      data,
-      "../templates/components/index.ejs",
-      `index.js`
-    )
-  ];
-  if (!data.flat) {
-    tasks.push(
-      createFileFromTemplate(
+const addComponent: (data: componentData) => Task = data =>
+  createFileFromTemplate(
+    data,
+    "../templates/components/Component.ejs",
+    `${data.name}.js`
+  );
+
+const addComponentStyled: (data: componentData) => Task = data =>
+  data.styled
+    ? createFileFromTemplate(
         data,
         "../templates/components/Component.ejs",
         `${data.name}.js`
       )
-    );
-  }
-  if (data.styled) {
-    tasks.push(
-      createFileFromTemplate(
+    : of(true);
+
+const addIndex: (data: componentData) => Task = data =>
+  data.flat
+    ? of(true)
+    : createFileFromTemplate(
         data,
         "../templates/components/ComponentStyled.ejs",
         `${data.name}Styled.js`
-      )
-    );
-  }
-  return makeDirsForPath(data.path).chain(_ =>
-    tasks.reduce((acc, task) => acc.and(task), of(true))
+      );
+
+const addFiles: (data: componentData) => Task = data =>
+  [addComponent(data), addComponentStyled(data), addIndex(data)].reduce(
+    (acc, task) => acc.and(task),
+    of(true)
   );
+
+export const createComponent: (data: componentData) => Task = data => {
+  return makeDirsForPath(data.path).chain(_ => addFiles(data));
 };
